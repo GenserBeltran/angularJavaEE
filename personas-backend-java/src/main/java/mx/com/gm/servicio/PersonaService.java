a0b2db484665ea858dc987c26fc9032499b849e1
Daniel Beltran <genserdanie-123@hotmail.com>
2022-09-10 21:11:10 +0200

    Creación_Frontend_Angular_pt1

---
+++ personas-backend-java/src/main/java/mx/com/gm/servicio/PersonaService.java
@@ -0,0 +1,19 @@
+package mx.com.gm.servicio;
+
+import java.util.List;
+import mx.com.gm.domain.Persona;
+
+public interface PersonaService {
+    
+    public List<Persona> listarPersonas();
+    
+    public Persona encontrarPersonaPorId(Persona persona);
+    
+    public Persona encontrarPersonaPorEmail(Persona persona);
+    
+    public void registrarPersona(Persona persona);
+    
+    public void modificarPersona(Persona persona);
+    
+    public void eliminarPersona(Persona persona);
+}
